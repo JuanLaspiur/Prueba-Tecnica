f880d195445f1c90ec0125370fda8cdf316b1a63
JuanLaspiur <laspiurperezjuan@gmail.com>
2024-07-30 16:19:51 -0300

    Guardar

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import ProductItem from "../components/ProductItem";
-import products from "../config/products.json"; // Ajusta la ruta según sea necesario
-import './indexStyles.css'; // Importa la hoja de estilos
+import products from "../config/products.json";
+import './indexStyles.css';
+import { FaSearch } from 'react-icons/fa';
 
 function Index() {
+  const [filter, setFilter] = useState("");
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filteredProducts = products.filter((product) =>
+    product.NombreProducto.toLowerCase().includes(filter.toLowerCase())
+  );
+
   const containerStyle = {
     color: "blue",
     backgroundColor: "lightgray",
@@ -17,18 +28,30 @@ function Index() {
   return (
     <div style={containerStyle}>
       <Header />
-      <h1>Lista de productos</h1>
+      <h1 className="listTitle">Lista de productos</h1>
+      <input
+        type="text"
+        placeholder="Buscar por nombre de producto"
+        value={filter}
+        onChange={handleFilterChange}
+        className="input-filter"
+      />
+         <FaSearch size={25} className="lupita" />
       <div className="listContainer">
-        {products.map((product, index) => (
-          <ProductItem
-            key={index}
-            NombreProducto={product.NombreProducto}
-            Descripcion={product.Descripcion}
-            DetalleDescripcion={product.DetalleDescripcion}
-            Imagen={product.Imagen}
-            ImagenMarca={product.ImagenMarca}
-          />
-        ))}
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product, index) => (
+            <ProductItem
+              key={index}
+              NombreProducto={product.NombreProducto}
+              Descripcion={product.Descripcion}
+              DetalleDescripcion={product.DetalleDescripcion}
+              Imagen={product.Imagen}
+              ImagenMarca={product.ImagenMarca}
+            />
+          ))
+        ) : (
+          <p> No tenemos ese producto</p>
+        )}
       </div>
     </div>
   );
